c8f5455b6a75bcdab3b12417748d10b9fc2bac1b
Gui <slgprogrammingguy@protonmail.com>
2025-11-08 11:43:51 -0300

    Adiciona e executa a seed

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    console.log("Rodando seed");
+    const py = await prisma.linguagem.create({
+        data:{nome:"Python"}
+    });
+
+    const ts = await prisma.linguagem.create({
+        data:{nome:"TypeScript"}
+    });
+
+    const user1 = await prisma.usuario.create({
+        data:{
+            email:"email@gmail.com",
+            senha:"abc",
+            nick:"usuer1",
+            Pontuacao:200,
+        }
+    });
+
+    const perguntas = await prisma.perguntas.create({
+        data:{
+            linguagem_id:py.id,
+            dificuldade:1,
+            pergunta:"Como se faz um dicionario em python",
+            alternativa:['{}', "dicionario()", "create dict"],
+            alt_correta:0
+        }
+    });
+
+    const respostaUsuario = await prisma.respostasDoUsuario.create({
+        data:{
+            usuario_id:user1.id,
+            perguntas_id:perguntas.id,
+            alt_selecionado:0,
+            acertou:true
+        }
+    });
+}
+
+main()
+    .catch(err => console.error(err))
+    .finally( async () => await prisma.$disconnect());
