a7c44aee82263535f20c3f85e84a348f674b7783
Gui <slgprogrammingguy@protonmail.com>
2025-11-16 09:52:24 -0300

    Adiciona as imagens das linguagens e cria os botoes de linguagem a partir de uma lista de nomes

--- src/app/linguagem/components/BotaoLinguagem.tsx
+++ src/app/linguagem/components/BotaoLinguagem.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 
-export default function BotaoLinguagem({ nome, children }: { nome: string, children: React.ReactNode }) {
+export default function BotaoLinguagem({ nome, posicao}: { nome: string, posicao: number }) {
     return (
         <button className="bg-white border-gray-200 border-2 h-50 w-55  m-2.5 rounded-xl shadow- hover:bg-gray-300 cursor-pointer">
-            <div className="flex flex-col items-center">
-                {children}
+            <div className="flex flex-col items-center" key={posicao}>
+                <Image src={"/" + nome + ".svg"}
+                    alt={nome + " logo"}
+                    width={70}
+                    height={70}
+                    className="">
+                </Image>
 
                 <h2 className="text-2xl m-5">{nome}</h2>
             </div>
--- src/app/linguagem/page.tsx
+++ src/app/linguagem/page.tsx
@@ -4,11 +4,30 @@ import SimpleNavbar from "./components/SimpleNavbar"
 import TabelaDeLinguagens from "./components/TabelaDeLinguas"
 
 export default function linguagem() {
+
+    const linguagens = ["Python",
+        "C",
+        "C++",
+        "JavaScript",
+        "Java",
+        "Go",
+        "PHP",
+        "Ruby",
+        "SQL",
+        "Swift",
+        "Bash",
+        "Kotlin",
+        "R",
+        "Scala",
+        "Rust",
+        "Lua"
+    ];
+
     return (
         <div className="flex flex-col h-screen">
             {/* https://ui.shadcn.com/docs/components/pagination# usar paginação para ir ao proximo item */}
             <SimpleNavbar />
-            <TabelaDeLinguagens />
+            <TabelaDeLinguagens linguagens={linguagens}/>
 
         </div>
     )
