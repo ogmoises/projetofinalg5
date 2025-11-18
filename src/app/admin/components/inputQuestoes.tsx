03fce1d58624e60d1b70304dca0e971100b33aa3
Gui <slgprogrammingguy@protonmail.com>
2025-11-18 16:47:11 -0300

    Adiciona prototipo da tabela de perguntas

---
+++ src/app/admin/components/inputQuestoes.tsx
@@ -0,0 +1,5 @@
+export default function inputQuestoes() {
+    return (
+        
+    )
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import TabelaDeQuestoes from "./components/tabelaDeQuestoes"
+
+export default function admin() {
+    return (
+        <div className="bg-branco h-screen p-2 flex justify-center">
+            <TabelaDeQuestoes/>
+        </div>
+    )
+}
